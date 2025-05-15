3ba3cddb54001f25888575721ebecb903bacd83c
Amaury Paillart <amaury.paillart@groupe-projex.fr>
2025-05-16 01:07:52 +0200

    Commented scraping

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ from scraper.scraper_custom import scrap_all_products
 if __name__ == "__main__":
     bookScraper = BookScraper()
 
-    products = scrap_all_products()
-    export_products_to_csv(products, 'data/products.csv')
-
     # Ces lignes sont commentées car j'ai la flemme de re scrap et re-exporter à chaque fois
     # L'affichage du nombre de livres totaux récupérés se trouve dans scraper_books.scrape_all()
 
     # total_books = bookScraper.scrape_all(50)
     # export_books_to_csv(total_books, "data/books.csv")
+
+    # products = scrap_all_products()
+    # export_products_to_csv(products, 'data/products.csv')
