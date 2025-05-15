877c3bf2acc5e7dc26d2fdce307c2075b66de0a5
Amaury Paillart <amaury.paillart@groupe-projex.fr>
2025-05-15 13:14:12 +0200

    Étape 6 complétée et notebook ajouté

--- analysis/stats.py
+++ analysis/stats.py
@@ -10,3 +10,30 @@ def load_books(filepath):
     )
 
     return books_df
+
+def describe_prices(df):
+    price_series = df['price']
+
+    description = {
+        'Prix moyen': price_series.mean(),
+        'Prix min': price_series.min(),
+        'Prix max': price_series.max(),
+        'Mediane': price_series.median(),
+        'Écart-type': price_series.std(),
+        '1er quartile': price_series.quantile(0.25),
+        '3eme quartile': price_series.quantile(0.75)
+    }
+
+    for key, value in description.items():
+        print(f"{key}: {value:.2f}")
+
+def availability_counts(df):
+    df2 = df.groupby(['availability', 'rating']).agg({
+        'rating': 'count',
+        'price': 'mean'
+    })
+
+    df2.rename(columns={'rating': 'count'}, inplace=True)
+    df2.rename(columns={'price': 'average price'}, inplace=True)
+
+    return df2
